ff81faca867881b511effee6e46f03da95ef7c43
Yasa <yasa@wigzo.com>
2022-04-18 17:18:13 +0530

    Added Multiply

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+
+public class Multiply {
+    
+    public static void main(String[]args){
+
+    int i = 8;
+    int j = 9;
+    int result = i*j;
+        System.out.println(result);
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,9 @@
+class Sum{
+    public static void main(String[]args){
+        int a = 2;
+        int b = 3;
+        int sum = a+b;
+
+        System.out.println("Sum is " +sum);
+    }
+}
